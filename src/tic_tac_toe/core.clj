64788f0ff44aa6214c7d88dbf4e2b79984c09400
JAP <j.prajzendanc@gmail.com>
2021-01-27 14:16:59 +0100

    zadania początkowe

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,10 +1,27 @@
 (ns tic-tac-toe.core)
 
+(def tablica [1 2 3 4 5 6 7 8 9])
+
 (defn stan
   "Wypisz stan gry"
-  [x y]
-  (println x "|" x "|" x)
+  [tab]
+  (println (nth tab 0) "|" (nth tab 1) "|" (nth tab 2))
   (println "---------")
-  (println y "|" x "|" y)
+  (println (nth tab 3) "|" (nth tab 4) "|" (nth tab 5))
   (println "---------")
-  (println "7" "|" "8" "|" "9"))
+  (println (nth tab 6) "|" (nth tab 7) "|" (nth tab 8)))
+
+(defn losuj
+  "Losuj gdzie komputer postawi X"
+  ;;sprawdź które numery wolne
+  )
+
+(defn start
+  "Rozpocznij grę"
+  [x]
+  ;;powitaj użytkownika
+  ;;pokaż mu stan gry
+  ;;poproś użytkownika o numer pola
+  ;;nadpisz to pole znakiem "O"
+  ;;pokaż użytkownikowi nowy stan
+  )
