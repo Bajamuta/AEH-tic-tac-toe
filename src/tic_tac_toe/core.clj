d7c0d8a109cb56763822325b49b2105f2c763bfd
JAP <j.prajzendanc@gmail.com>
2021-01-28 11:16:10 +0100

    Losuj nr pola gdzie komputer postawi X

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,6 +1,7 @@
 (ns tic-tac-toe.core)
 
 (def tablica [1 2 3 4 5 6 7 8 9])
+(def tab2 [1 2 3 0 0 0 0 0 0])
 
 (defn stan
   "Wypisz stan gry"
@@ -11,9 +12,20 @@
   (println "---------")
   (println (nth tab 6) "|" (nth tab 7) "|" (nth tab 8)))
 
+(defn wolne
+  "Sprawdź które pola są wolne"
+  [tab]
+  (filter some? (for [y (range 0 (count tab))]
+    (if-let [x (= (nth tab y) (nth tablica y))]
+      y)
+    ))
+  )
+
 (defn losuj
   "Losuj gdzie komputer postawi X"
-  ;;sprawdź które numery wolne
+  [tab]
+  ;;zwróć wylosowany numer pola, gdzie komputer postawi "X"
+  (nth (wolne tab) (rand-int (count (wolne tab))))
   )
 
 (defn start
