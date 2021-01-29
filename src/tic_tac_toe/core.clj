6440e938ef8b3734238551da49ef15db005dde08
JAP <j.prajzendanc@gmail.com>
2021-01-29 11:07:56 +0100

    pierwsza runda z wyświetleniem stanu

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -4,12 +4,6 @@
 (def tab-gra [1 2 3 4 5 6 7 8 9])
 (def tab2 [1 2 3 0 0 0 0 0 9])
 
-(def runda1 (x))                                            ;;zapisz stan rundy1
-(def runda2 (x))
-(def runda3 (x))
-(def runda4 (x))
-(def runda5 (x))
-
 (defn stan
   "Wypisz stan gry"
   [tab]
@@ -23,23 +17,50 @@
   "Sprawdź które pola są wolne; a to numer pola które wybrał użytkownik"
   [tab a]
   (filter some? (for [y (range 0 (count tab))]
-    (if (not (= (- a 1) y))
-      (if (= (nth tab y) (nth tablica y)) y))
+    (if (not (= a y))
+      (if (= (nth tab y) (nth tablica y)) y)
+      nil)
     ))
   )
 
 (defn losuj
   "Losuj gdzie komputer postawi X; a to numer pola które wybrał użytkownik"
-  [tab a]
-  ;;zwróć wylosowany numer pola, gdzie komputer postawi "X"
-  (nth (wolne tab a) (rand-int (count (wolne tab a))))
-  )
+  [tab]
+  ;;zwróć wylosowany index numeru pola, gdzie komputer postawi "X"
+  (rand-int (count tab))
+)
+
+(defn komputer [tab x]
+  (println "Komputer zagrał pole: " (+ x 1))
+  (nth tab x))
+
+(wolne tablica 2)
+(rand-int (count (wolne tablica 2)))
+(nth (wolne tablica 2) 4)
 
 (defn nadpisz
   "Zaktualizuj tablicę gry, x to pozycja gracza, y to pozycja komputera"
   [x y tab]
-  (for [z (range 0 (count tab))]
-    (if (= z (- x 1)) "O" (if (= z (- y 1)) "X" (nth tab z))))
+  ;;(println x y)
+  (if (not (= x y))
+    (for [z (range 0 (count tab))]
+      (do
+        (if (= z x)
+          "O"
+          (if (= z y)
+            "X"
+            (nth tab z)))
+        )
+      )
+    (println "Błąd"))
+  )
+
+(defn index [a]
+  (- (Long/parseLong a) 1)
+  )
+
+(defn runda [a]
+  (nadpisz a (komputer (wolne tablica a) (losuj (wolne tablica a))) tablica)
   )
 
 (defn start
@@ -50,5 +71,6 @@
   ;;poproś użytkownika o numer pola
   ;;nadpisz to pole znakiem "O"
   ;;pokaż użytkownikowi nowy stan
-  (nadpisz x (losuj tablica x) tablica)
+  (println "Podaj numer pola:")
+  (stan (runda (index (do (flush) (read-line)))))
   )
